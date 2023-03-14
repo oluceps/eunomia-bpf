1edaf8d66ce3c7728f6d6d7aec116887b2c89398
oluceps <i@oluceps.uk>
2023-03-14 23:42:27 +0800

    ecli: add server api
    This commit adds openAPI style server APIs and
    client implementations.
    
    add documents for ecli api codegen

--- ecli/src/main.rs
+++ ecli/src/main.rs
@@ -5,13 +5,13 @@
 //!
 mod config;
 mod error;
-mod wasm_bpf_runner;
 mod json_runner;
 mod oci;
 mod runner;
 mod tar_reader;
 use signal_hook::{consts::SIGINT, iterator::Signals};
-use std::{thread, process};
+use std::{process, thread};
+mod wasm_bpf_runner;
 use clap::{Parser, Subcommand};
 use env_logger::{Builder, Target};
 use error::EcliResult;
@@ -19,7 +19,8 @@ use oci::{
     auth::{login, logout},
     pull, push,
 };
-use runner::run;
+// use runner::start_server;
+use runner::{client_action, run, start_server};
 
 #[derive(Subcommand)]
 pub enum Action {
@@ -31,6 +32,21 @@ pub enum Action {
         #[arg(allow_hyphen_values = true)]
         prog: Vec<String>,
     },
+
+    #[clap(name = "server", about = "start a server to control the ebpf programs")]
+    Server {
+        #[arg(short, long)]
+        config: Option<String>,
+        #[arg(short, long, default_value = "false")]
+        secure: bool,
+        #[clap(short, long, help = "server port", default_value = "8527")]
+        port: u16,
+        #[arg(short, long, default_value = "127.0.0.1")]
+        addr: String,
+    },
+
+    #[clap(name = "client", about = "Client operations")]
+    Client(ClientCmd),
 
     Push {
         #[arg(long, short, default_value_t = ("app.wasm").to_string())]
@@ -63,6 +79,53 @@ struct Args {
     action: Action,
 }
 
+#[derive(Parser)]
+pub struct ClientCmd {
+    #[clap(subcommand)]
+    cmd: ClientSubCommand,
+
+    #[clap(flatten)]
+    opts: ClientOpts,
+}
+
+#[derive(Parser)]
+enum ClientSubCommand {
+    #[clap(name = "start", about = "start an ebpf programs on endpoint")]
+    Start(StartCommand),
+
+    #[clap(name = "stop", about = "stop running tasks on endpoint with id")]
+    Stop(StopCommand),
+
+    #[clap(name = "list", about = "list the ebpf programs running on endpoint")]
+    List,
+}
+
+#[derive(Parser)]
+struct ClientOpts {
+    #[clap(short, long, help = "endpoint", default_value = "127.0.0.1")]
+    endpoint: String,
+
+    #[clap(short, long, help = "enpoint port", default_value = "8527")]
+    port: u16,
+
+    #[clap(short, long, help = "transport with https", default_value = "false")]
+    secure: bool,
+}
+
+#[derive(Parser)]
+struct StartCommand {
+    #[clap(required = true)]
+    prog: Vec<String>,
+    #[clap(long)]
+    extra_args: Option<Vec<String>>,
+}
+
+#[derive(Parser)]
+struct StopCommand {
+    #[clap(required = true)]
+    id: Vec<i32>,
+}
+
 fn init_log() {
     let mut builder = Builder::from_default_env();
     builder.target(Target::Stdout);
@@ -72,18 +135,16 @@ fn init_log() {
 #[tokio::main]
 async fn main() -> EcliResult<()> {
     let signals = Signals::new(&[SIGINT]);
-    thread::spawn(move || {
-        match signals {
-            Ok(mut signals_info) => {
-                for sig in signals_info.forever() {
-                    println!("Received signal {:?}", sig);
-                    process::exit(0);
-                }
-                println!("Got signals info: {:?}", signals_info);
-            },
-            Err(error) => {
-                eprintln!("Error getting signals info: {}", error);
+    thread::spawn(move || match signals {
+        Ok(mut signals_info) => {
+            for sig in signals_info.forever() {
+                println!("Received signal {:?}", sig);
+                process::exit(0);
             }
+            println!("Got signals info: {:?}", signals_info);
+        }
+        Err(error) => {
+            eprintln!("Error getting signals info: {}", error);
         }
     });
     init_log();
@@ -94,6 +155,7 @@ async fn main() -> EcliResult<()> {
         Action::Pull { .. } => pull(args.action.try_into()?).await,
         Action::Login { url } => login(url).await,
         Action::Logout { url } => logout(url),
+        Action::Client(..) => client_action(args.action.try_into()?).await,
+        Action::Server { .. } => start_server(args.action.try_into()?).await,
     }
-    
 }
