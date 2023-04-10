ce1c3573da3e4901c80fc56626b1df8d59b852cd
oluceps <i@oluceps.uk>
2023-04-10 16:09:40 +0800

    ecli(lib): add test

--- ecli/lib/src/runner/mod.rs
+++ ecli/lib/src/runner/mod.rs
@@ -205,8 +205,8 @@ pub async fn start_server(args: RemoteArgs) -> EcliResult<()> {
         port, addr, secure, ..
     } = args.server.unwrap()
     {
-        println!("starting server...");
         let addr: String = format!("{addr}:{port}");
+        println!("Server start at {addr}");
         let (_tx, shutdown_rx) = tokio::sync::oneshot::channel::<()>();
 
         server::create(addr, secure, shutdown_rx).await;
@@ -264,7 +264,8 @@ pub async fn client_action(args: RemoteArgs) -> EcliResult<()> {
     match action_type {
         ClientActions::List => {
             let result = client.list_get().await;
-            println!("{}", json!(result.as_ref().unwrap()));
+            let ListGetResponse::ListOfRunningTasks(rsp_msg) = result.as_ref().unwrap();
+            println!("{}", json!(rsp_msg));
             info!(
                 "{:?} (X-Span-ID: {:?})",
                 result,
@@ -301,7 +302,9 @@ pub async fn client_action(args: RemoteArgs) -> EcliResult<()> {
                 )
                 .await;
 
-            println!("{}", json!(result.as_ref().unwrap()));
+            let StartPostResponse::ListOfRunningTasks(rsp_msg) = result.as_ref().unwrap();
+
+            println!("{}", json!(rsp_msg));
             info!(
                 "{:?} (X-Span-ID: {:?})",
                 result,
@@ -318,7 +321,9 @@ pub async fn client_action(args: RemoteArgs) -> EcliResult<()> {
                 };
 
                 let result = client.stop_post(inner).await;
-                println!("{}", json!(result.as_ref().unwrap()));
+
+                let StopPostResponse::StatusOfStoppingTheTask(rsp_msg) = result.as_ref().unwrap();
+                println!("{}", json!(rsp_msg));
                 info!(
                     "{:?} (X-Span-ID: {:?})",
                     result,
